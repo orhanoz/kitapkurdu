cb7933654d7822725e78e35f490bb9ec6ad549a9
Ramazan Demir <ramazandemir0@arcelik.com>
2020-02-11 19:30:20 +0300

    sample tempate website added

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {page:'Home', menuId:'home'});
 });
+
+router.get('/about', function(req, res, next) {
+  res.render('about', {page:'About Us', menuId:'about'});
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', {page:'Contact Us', menuId:'contact'});
+});
+
+
 
 module.exports = router;

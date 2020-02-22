19d7df4e6baa7bfe3648740fa9abd377368cee9b
Orhan Özbek <orhanoz91@gmail.com>
2020-02-22 16:28:43 +0300

    hop
    kokorec

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
 app.use('/login', require('./routes/login'));
+app.use('/isbn', require('./routes/isbn'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

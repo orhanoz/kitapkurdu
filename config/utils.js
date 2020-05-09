659e9a378c5d1ec4914969e75c327848abc50d38
Ramazan Demir <ramazandemir0@arcelik.com>
2020-05-09 22:45:13 +0300

    development domain updated!

--- config/utils.js
+++ config/utils.js
@@ -8,7 +8,7 @@ var crypto = require('crypto');
 
 module.exports = {
 
-    domain: "https://kitapkurduu.herokuapp.com",
+    domain: "http://localhost:3000", // "https://kitapkurduu.herokuapp.com",
 
     api_key: "",
 

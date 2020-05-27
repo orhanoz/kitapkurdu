a859a5554065f74ea0c06c5f37a6d398ff8d6503
Ahmet DAĞTAŞ <adagtas@innova.com.tr>
2020-05-28 00:08:38 +0300

    smtp adres düzeltme

--- config/utils.js
+++ config/utils.js
@@ -29,7 +29,7 @@ module.exports = {
 
         
         let config = {
-            host: 'smtp.yandex.com',
+            host: 'smtp.yandex.ru',
             port: 465,
             secure: true,
             auth: {
